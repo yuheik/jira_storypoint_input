4b2024b184d6dc0108fb67e17bd4dbcdb5f8bd52
yuhei.k <yuhei.k@gmail.com>
2020-04-17 02:00:41 +0900

    Squashed 'lib/jira/' changes from 527f49a..6ecdb4f
    6ecdb4f Change api call methods into class method.
    5afde09 Refactoring jira_accessor. Separate api call sequence.
    
    git-subtree-dir: lib/jira
    git-subtree-split: 6ecdb4fe2a945c75ed89a5be4a671ea74704a031

--- jira_accessor.rb
+++ jira_accessor.rb
@@ -1,11 +1,11 @@
 # coding: utf-8
 
-require 'rest-client'
 require 'json'
+require_relative './api_caller_restclient'
 require_relative './my_credential'
 
-class JiraAccessor
-  API_URL = "#{Credential::Site}/rest/api/2"
+class JiraAccessor < ApiCallerRestClient
+  JIRA_URL = "#{Credential::Site}/rest/api/2"
 
   def self.compose_payload(story_point)
     param = Hash.new
@@ -21,35 +21,10 @@ class JiraAccessor
   # 'JIRA Settings' => 'Screens' => 'Default Issue Screen' Edit => 'Field Tab'.
   # Add 'Story Points' field there.
   def self.update_issue(key, story_point)
-    begin
-      response = RestClient::Request.new(:method     => :PUT,
-                                         :url        => "#{API_URL}/issue/#{key}",
-                                         :user       => Credential::UserName,
-                                         :password   => Credential::Password,
-                                         :proxy      => nil,
-                                         :verify_ssl => false,
-                                         :payload    => compose_payload(story_point),
-                                         :headers    => { :content_type => "application/json;charset=UTF-8" }
-                                        ).execute
-    rescue RestClient::ExceptionWithResponse => e
-      puts "Error: (#{e.message} with #{e.response})"
-      puts "Failed: #{e.backtrace}"
-      # puts e
-    end
-
-    # dump(response)
-  end
-
-  def self.dump(response)
-    abort if response.class != RestClient::Response
-
-    puts "Dump Response --"
-    puts "code:       #{response.code}"
-    puts "body:       #{response.body}"
-    puts "headers:    #{JSON.pretty_generate(response.headers)}"
-    puts "cookies:    #{JSON.pretty_generate(response.cookies)}"
-    puts "cookie_jar: #{response.cookie_jar.inspect}"
-    puts "request:    #{response.request.inspect}"
-    puts "history:    #{response.history}"
+    call("#{JIRA_URL}/issue/#{key}",
+         Credential::UserName,
+         Credential::Password,
+         :PUT,
+         compose_payload(story_point))
   end
 end
